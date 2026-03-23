e674d5199fa9c8de5393cf0c57f2837f6de1444c
mohamed amdouni <midouamdouni4@gmail.com>
2026-03-23 22:53:31 +0000

    removed unecessary headers

--- fonctions.c
+++ fonctions.c
@@ -80,6 +80,147 @@ void insert(int table[],int size,inserty* i)
        
 }
 
+void merge(int table[],int size, mergy* m){
+    if (m->finished){
+        return;
+    }
+    if(m->stack_top=-1){
+        m->finished=true;
+        return;
+    }
+    m->left=m->stack[m->stack_top].left;
+    m->right=m->stack[m->stack_top].right;
+    m->phase=m->stack[m->stack_top].left;
+    if (m->left==m->right){
+        m->stack_top--;
+        return;
+    }
+    m->mid=(m->left+m->right)/2;
+    if (m->phase==0){
+        m->stack[m->stack_top].phase=1;
+        m->stack_top++;
+        m->stack[m->stack_top].left=m->left;
+        m->stack[m->stack_top].right=m->mid;
+        m->stack[m->stack_top].phase=0;
+    }else if(m->phase==1){
+        m->stack[m->stack_top].phase=2;
+        m->stack_top++;
+        m->stack[m->stack_top].left=m->mid+1;
+        m->stack[m->stack_top].right=m->right;
+        m->stack[m->stack_top].phase=0;
+    }else if(m->phase==2){
+        m->i = m->left;
+        m->j =m->mid + 1;
+        m->k =0;
+        m->subphase =2.0;
+        if(merge_step(table,m)){
+            m->stack_top--;
+        }
+    }}
+
+
+void merge_step(int table[],mergy* m){
+    
+    if (m->subphase==2.0){
+        if(m->i>m->mid || m->j>m->right){
+            if(m->i>m->mid){
+                m->subphase=2.1;
+                return;
+
+            }else if(m->j>m->right){
+                m->subphase=2.2;
+                return;
+            }
+        }
+        if (table[m->i]<table[m->j]){
+            m->temp[m->k]=table[m->i];
+            m->i++;
+            m->k++;
+        }else{
+            m->temp[m->k]=table[m->j];
+            m->j++;
+            m->k++;
+        }
+        return;
+    }else if (m->subphase==2.1){
+        m->temp[m->k]=table[m->i];
+        m->i++;
+        m->k++;
+        m->temp_size++;
+        if(m->i>m->mid){
+            m->k=0;
+            m->subphase=2.3;}
+        
+        return;
+    }else if (m->subphase==2.2){
+        m->temp[m->k]=table[m->j];
+        m->j++;
+        m->k++;
+        m->temp_size++;
+        if(m->j>m->right){
+            m->k=0;
+            m->subphase=2.3;}
+    return;}
+    else if (m->subphase==2.3){
+        table[m->left+m->k]=m->temp[m->k];
+        m->k++;
+
+    }
+
+
+}
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- headers.h
+++ headers.h
@@ -32,8 +32,3 @@ typedef struct {
     int j;
     bool finished ;
     }bubbly;
-void insert(int table[],int size,inserty* i);
-void resetem(bubbly *bub,selecty* selectful,inserty* insertful,bool *bubbles,bool *select,bool *insert);
-void bubble(int table[],int size,bubbly *bubblestate);
-bool checkmouse(int mx,int my,int lb,int rb,int tb,int bb);
-void selection(int table[],int size,selecty* select);
